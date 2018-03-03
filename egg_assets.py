7c4896f92a6a64614f2a861b8c6e5f1f31d459cd
Clayton Besaw <claybesaw@gmail.com>
2018-03-03 10:07:24 -0700

    egg function assets

---
+++ egg_assets.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 20:20:33 2018
+Assets for egg bot
+
+@author: Claytonious
+"""
+
+####asset lists
+
+#greeting text
+greet_txt = '''Welcome to Egg-Server young hatchling!
+                     ------------------------------------------
+                     My creator Claytonious gave me a number of 
+                     useless commands, and at least one easter egg...
+                     -----------------------------------------------------------------
+                     Use the prefix "egg-" to use the following commands
+                     -----------------------------------------------------------------
+                     Commands:
+                     1) greet - Pulls up this screen!    
+                     2) test - Tests the eggcasting service.
+                     3) users - Gives user information about Egg-Server inhabitants.
+                     4) fortune - Provides the user with a glimpse into their future.
+                     5) boost - Ask Egg Bot for a confidence boost!
+                     6) tulku - Only ask Egg Bot this if you are tulku'd right well bro.   
+                     ----------------------------------------------------------------------
+                     Feel free to look at my source code: https://github.com/cbesaw/egg-bot
+                     ----------------------------------------------------------------------
+                     Planned functionality:
+                         1) Near future - NeuralNet text generation trained on egg server chat logs.
+                     ----------------------------------------------------------------------
+                     Please suggest any new features to my creator!
+                     '''
+
+tulku_memes = ['https://my420tours.com/wp-content/uploads/2017/12/weed-memes-2.jpg',
+                   'https://my420tours.com/wp-content/uploads/2017/12/weed-memes-3.png',
+                   'https://my420tours.com/wp-content/uploads/2017/11/weed-measurements-meme-e1512520782753.jpg',
+                   'https://www.youtube.com/watch?v=HQzteyXbqNg', 'https://my420tours.com/wp-content/uploads/2017/11/weed-measurements-meme-e1512520782753.jpg',
+                   'https://my420tours.com/wp-content/uploads/2017/12/weed-memes-6.jpg',
+                   'https://my420tours.com/wp-content/uploads/2017/12/weed-memes-8.5.jpg',
+                   'https://my420tours.com/wp-content/uploads/2017/12/weed-memes-11.jpg',
+                   'https://my420tours.com/wp-content/uploads/2017/12/weed-memes-14.jpg',
+                   'https://my420tours.com/wp-content/uploads/2017/12/weed-memes-16.jpg',
+                   'https://my420tours.com/wp-content/uploads/2017/12/weed-memes-20.jpg',
+                   'https://img.huffingtonpost.com/asset/56ba204f1800006f0080b911.jpeg?cache=m1lwltw97o&ops=scalefit_970_noupscale',
+                   'https://img.huffingtonpost.com/asset/56ba204f1800002d0080b910.jpeg?ops=scalefit_970_noupscale',
+                   'https://img.huffingtonpost.com/asset/56ba204d1a00002d00ab2471.jpeg?ops=scalefit_970_noupscale',
+                   'https://img.huffingtonpost.com/asset/56ba204e1a00009c01ab2472.jpeg?ops=scalefit_970_noupscale',
+                   'https://img.huffingtonpost.com/asset/56ba204f1a00009c01ab2473.jpeg?ops=scalefit_970_noupscale',
+                   'https://img.huffingtonpost.com/asset/56ba204f1f00000d01217a16.jpeg?ops=scalefit_970_noupscale',
+                   'http://cryptomundo.com/wp-content/uploads/yetinazis.jpg',
+                   'https://pics.me.me/when-youre-extremely-high-in-public-but-holding-it-together-18001221.png']
+
+#black widow
+
+bw_text = '''
+                     Help me in my search for knowledge,
+                     I must learn the secret art.
+                     Who dares to help me raise the on
+                     Whose very name near stills my heart?
+                     Astaroth!!
+                     Discard your clothes and come on foot,
+                     Through streams and fields and moonlit moors,
+                     Your bodies soaked in sacred oils,
+                     Perfumed herbs will heal your sores.
+                     Join me in my search for power.
+                     Wives and husbands bring your kin.
+                     We'll be as one within the hour.
+                     Let the sabbat now begin.
+                     Come, come, come to the sabbat.
+                     Come, come, come to the sabbat.
+                     Come, come, come to the sabbat.
+                     Come, come, come to the sabbat.
+                     Come, come, come to the sabbat.
+                     Come, come, come to the sabbat.
+                     Come to the sabbat, Satan´s there! 
+                     '''
+#fortunes
+fortune_list = ['Your future is most eggcellent!', 'You are eggsactly where you are supposed to be!',
+                    'Eggciting things in your future!', 'For a long life, eat eggs and get some eggercise.',
+                    'Eggstreme success in your future', 'Eggsplosive news coming your way', 'Eggstatic news coming!',
+                    'You are walking on egg-shells my friend.', 'Eggstreme hardships coming friend.', 'Your future is simply eggstravagent.',
+                    'Eggceptionally good luck coming your way.', 'Eggceptionally bad luck coming your way.', 'A rotten egg comes your way...',
+                    'Your weekend will be scrambled.', 'Your future looks hard boiled friend.', 'Don\'t egg-nore the good news coming to you soon.',
+                    'Simply....EGGCELLENT!', 'You will need to break a few eggs soon.']
